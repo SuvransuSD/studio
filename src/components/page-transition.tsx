34e44f70d257d8407469d8fb25bf4d3d502eca36
Nikhil Ramdas Chavan <nikhil.codestudio@gmail.com>
2025-09-10 18:43:25 +0000

    just implement loading animation

--- src/components/page-transition.tsx
+++ src/components/page-transition.tsx
@@ -54,16 +54,7 @@ const TransitionInner = ({ children }: { children: ReactNode }) => {
 
   return (
     <AnimatePresence mode="wait">
-       {loading && <PageLoader />}
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
-      >
-        {children}
-      </motion.div>
+       {loading ? <PageLoader /> : children}
     </AnimatePresence>
   );
 };
